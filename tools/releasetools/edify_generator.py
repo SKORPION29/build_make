1e43998bda21efae91bb94e913eb078bc285867c
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-03-03 14:55:31 +0530

    ProjectBlaze Flashing Script
    Signed-off-by : afterallafk <shivamatiet2001@gmail.com>

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -268,11 +268,11 @@ class EdifyGenerator(object):
                                   security_patch, device):
     self.Print("----------------------------------------------")
     if is_plus:
-      self.Print("        PixelExperience (Plus edition)")
-      self.Print("                by jhenrique09")
+      self.Print("               ProjectBlaze")
+      self.Print("              by Aditya Singh")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("               ProjectBlaze")
+      self.Print("              by Aditya Singh")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
